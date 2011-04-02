fdc42982fcfaead9f1b2a27406380425bf03edec
Michael <midpeter444@gmail.com>
2011-04-02 13:16:25 -0400

    Finished Ch. 3 - static pages.  All tests passing.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
+  # just passes through to the view (home.html.erb)
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "Contact"
   end
 
+  def about
+    @title = "About"
+  end
 end
