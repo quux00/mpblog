7070f8780a79224c591c33e5ff050db9d0595473
Michael <midpeter444@gmail.com>
2011-04-02 15:56:34 -0400

    End of ch.4 (not much happened with the code in this chapter).
    Added the CSS-blueprint library.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
+# Note this is a module, not class
+# that allows the controllers to include the module
+# and make the helper methods availble as if they
+# were instance methods.
+# This helper module is included for all controllers
+# so any methods defined here will be automatically 
+# available in all view
 module ApplicationHelper
+  # return title on per page basis
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #@title"
+    end
+  end
 end
