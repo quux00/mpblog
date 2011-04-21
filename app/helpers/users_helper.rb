0edc363b6df3bfdcb992d6aefb5fe28c031a6f19
Michael <midpeter444@gmail.com>
2011-04-20 20:37:36 -0400

    Final commit - ch. 7
    Swapped out real gravatars for my local gecko image.  Reran annotate
    on the user model to get it up to date.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,14 @@
 module UsersHelper
   def gravatar_for(user, options = {:size => 50})
-    gravatar_image_tag(user.email.downcase, 
-                       :alt => user.name,
-                       :class => 'gravatar',
-                       :gravatar => options)
+    # replacement 'local' code instead
+    image_tag('gecko-logo.jpg', 
+              :class => 'gravatar',
+              :alt => user.name);
+              
+    # I don't actually want to use this so I can run tests while offline
+    # gravatar_image_tag(user.email.downcase, 
+    #                    :alt => user.name,
+    #                    :class => 'gravatar',
+    #                    :gravatar => options)
   end
 end
