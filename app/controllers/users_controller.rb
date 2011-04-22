57fdce26f120366563641f0416f2a6f96ba9ed11
Michael <midpeter444@gmail.com>
2011-04-22 01:23:03 -0400

    End of ch. 8 - interim, as not done exercises yet.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
     @title = "Sign up"
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if (@user.save)
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # render user_path(@user)  #~TODO: is this synonymous to the line above?
+                                 # answer: no - render and redirect do something different
+                                 # the render version does not pass the unit test
+    else
+      @title = "Sign up"
+      render new_user_path
+      # render 'new'    #~TODO: is this synonymous to the line above?
+    end
   end
 
   def show
